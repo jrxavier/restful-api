c514cda43a0d02016f0f3d3b81e7c3a29744e1ea
Jose Xavier <jrxavier@gmail.com>
2019-04-14 19:32:48 -0300

    Recuperando dados

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+const db = mongoose.connect('mongodb://127.0.0.1:27017/bookAPI');
+const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Welcome to my node app');
-});
+const Book = require('./models/bookModel');
+
+bookRouter.route('/books')
+    .get((req, res) => {
+        const query = {};
+
+        if (req.query.genre) {
+            query.genre = req.query.genre;
+        }
+
+        Book.find(query, (err, books) => {
+            if (err) {
+                return res.send(err);
+            }
+            return res.send(books);
+
+        });
+    });
+
+bookRouter.route('/books/:bookId')
+    .get((req, res) => {
+        Book.findById(req.params.bookId, (err, books) => {
+            if (err) {
+                return res.send(err);
+            }
+            return res.send(books);
+
+        });
+    });
+
+app.use('/api', bookRouter);
 
 
 app.listen(port, () => {
---
+++ booksJson.js
@@ -0,0 +1,25 @@
+db.books.insert([{
+        title: "War and Peace",
+        genre: "Ficçao ",
+        author: "Tolstoy",
+        read: false
+    },
+    {
+        title: "Les Miserablés",
+        genre: "Ficção",
+        author: "Vitor Hugo",
+        read: false
+    },
+    {
+        title: "`Sitio do Picapau Amarelo",
+        genre: "Literatura infantil",
+        author: "Monteiro Lobato",
+        read: false
+    },
+    {
+        title: "Livro dos Esp[iritos",
+        genre: "Religão",
+        author: "Allan Kardec",
+        read: true
+    }
+]);
