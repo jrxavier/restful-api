8c3083c860dee2923718acc0198fd3d011b4791d
Jose Xavier <jrxavier@gmail.com>
2019-04-14 20:24:38 -0300

    Envio de dados

--- app.js
+++ app.js
@@ -1,44 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 const db = mongoose.connect('mongodb://127.0.0.1:27017/bookAPI');
-const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
-
 const Book = require('./models/bookModel');
+const bookRouter = require('./routes/bookRouter')(Book);
 
-bookRouter.route('/books')
-    .get((req, res) => {
-        const query = {};
-
-        if (req.query.genre) {
-            query.genre = req.query.genre;
-        }
-
-        Book.find(query, (err, books) => {
-            if (err) {
-                return res.send(err);
-            }
-            return res.send(books);
-
-        });
-    });
-
-bookRouter.route('/books/:bookId')
-    .get((req, res) => {
-        Book.findById(req.params.bookId, (err, books) => {
-            if (err) {
-                return res.send(err);
-            }
-            return res.send(books);
-
-        });
-    });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use('/api', bookRouter);
 
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+    console.log(`Running on port ${port}`);
 });
