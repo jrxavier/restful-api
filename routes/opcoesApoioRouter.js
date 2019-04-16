331ca8030bfd07b37e75c5263f55781ccaa4e90b
Jose Ricardo Xavier <jrxavier@bndes.gov.br>
2019-04-16 17:53:11 -0300

    Ajuste para retorno da pesquisa de Opcoes de Apoio

--- routes/opcoesApoioRouter.js
+++ routes/opcoesApoioRouter.js
@@ -1,14 +1,54 @@
 const express = require('express');
+const {
+   createApolloFetch
+} = require('apollo-fetch');
 
 function routes() {
    const opcoesRouter = express.Router();
+   const fetch = createApolloFetch({
+      uri: 'http://desis-docker02.cloud.dsv.bndes.net:4000/api'
+   });
+   var retorno = null;
+   const query = `
+      query Opcoes($vPorte: String) {
+         opcoesApoio(porte: $vPorte) { 
+            identificador
+            titulo
+            taxajurostotal
+            participacaomaxima
+            prazototalmaximo
+            limitemaximo
+            limitemaximodesc
+            prazomaximo_desc
+            porte
+            pessoaFisica
+            pessoaJuridica             
+         }
+      }`
    opcoesRouter.route('/opcoesApoio')
       .get((req, res) => {
-         const obj = {
-            nome: 'Jose'
-         };
-         console.log('teste');
-         return res.status(201).json(obj);
+         const param = {}
+
+         if (req.query.porte) {
+            param.porte = req.query.porte;
+            console.log(param.porte);
+         }
+         let variables = {};
+         variables.vPorte = param.porte;
+         console.log("---> " + variables.vPorte);
+         fetch({
+               query,
+               variables
+            })
+            .then(result => {
+               console.log("Valor de retorno: " + result.data);
+               retorno = result.data.opcoesApoio;
+               return res.status(201).json(retorno);
+            })
+            .catch(error => {
+               console.log("Error: " + error);
+            });
+
       });
 
    return opcoesRouter;
